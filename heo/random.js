99ecfebb66b3c35a46c33752d8f1b16f687da262
wzwzx <antidooo@163.com>
2023-03-25 17:07:32 +0800

    Site updated: 2023-03-25 17:07:26

---
+++ heo/random.js
@@ -0,0 +1,1 @@
+var posts=["/post/about-gitalk.html","/post/annual-summary-2022.html","/post/begonia-krsys.html","/post/Yunbianyougexiaomaibu.html","/post/keqing-shuai.html","/post/new-blog.html","/post/silly-wisher-chuangsiwo.html","/post/aurora-music.html"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
